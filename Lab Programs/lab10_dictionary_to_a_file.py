69d7c75cb8877696c8b739df5a84c4a1cd88b74c
ksheena <50112191+ksheena@users.noreply.github.com>
2024-09-13 13:18:01 +0530

    Delete Lab Programs directory

--- Lab Programs/lab10_dictionary_to_a_file.py
+++
@@ -1,7 +0,0 @@
-mydict = {'fname' : "kiran", 'lname' : "kumar", 'dep' : "ACDC"}
-
-
-fo=open('c:\\Users\\k3bywp8\\text3.txt','w') 
-for key, value in mydict.items():
-    fo.write('%s:%s\n' % (key, value))
-
